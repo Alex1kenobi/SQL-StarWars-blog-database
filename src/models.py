ffcdce70619d845d92843f5dc7338e9f5e8b65cd
Alex1kenobi <alebarto2002@gmail.com>
2022-08-11 18:56:30 +0000

    Backend terminado con diagram.png

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,45 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    password = Column(String(25), nullable=False)
+    email = Column(String(250), nullable=False)
+    Sing_up_date = Column(DateTime, nullable=False)
+    Fk_favourist_list = Column(Integer, ForeignKey ('favourite.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Favourite(Base):
+    __tablename__ = 'favourite'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    character_list = relationship("Character")
+    planet_list = relationship("Planets")
+    user_id = relationship("User")
 
-    def to_dict(self):
-        return {}
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    Name = Column(String(250))
+    Specie = Column(String(250))
+    Birth = Column(String(250))
+    favourite_id = Column(Integer,ForeignKey('favourite.id'))
 
-## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+
+class Planets(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    Name = Column(String(250))
+    Kind = Column(String(250))
+    favourite_id = Column(Integer,ForeignKey('favourite.id'))
+
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
